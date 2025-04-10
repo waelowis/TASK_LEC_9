6ec68015d01e2f0886ee2ff758987dd4b84460e6
wael <163440911+waelowis@users.noreply.github.com>
2025-04-10 16:09:51 +0300

    Add project files.

---
+++ TASK_LEC_9/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TASK_LEC_9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
